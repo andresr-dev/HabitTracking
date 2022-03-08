ea55c7a2ca45a861d3bf4b3acfdb8473777f787b
Andres <andresr774@gmail.com>
2022-03-08 15:49:54 -0500

    Initial Commit

---
+++ HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Andres camilo Raigoza misas on 8/03/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
